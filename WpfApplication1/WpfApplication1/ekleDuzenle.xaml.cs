f9e3d48d0f6f41c4ee3a8d4ca780a6efc1c6109f
astroids <murat5555588@mynet.com>
2013-11-16 20:47:16 +0200

    try catch

--- WpfApplication1/WpfApplication1/ekleDuzenle.xaml.cs
+++ WpfApplication1/WpfApplication1/ekleDuzenle.xaml.cs
@@ -52,62 +52,72 @@ namespace WpfApplication1
         private void kaydetbutonu_Click(object sender, RoutedEventArgs e)
         {
 
-
-
-            if (x == 1)
+            try
             {
- 
-                if (con.State == ConnectionState.Closed)
+
+                if (x == 1)
                 {
-                    con.Open();
-                }
-                cmd.Connection = con;
-                cmd.CommandText = @"Insert Into Tbl_Personel(P_Adi,P_Soyadi,P_TcKimlik,P_Tel1,P_Tel2,P_Email,P_Cinsiyet,P_D_Tar,P_D_Yer,P_Pozisyon,P_Dept,P_Med_Hal,P_Aday) 
+
+                    if (con.State == ConnectionState.Closed)
+                    {
+                        con.Open();
+                    }
+                    cmd.Connection = con;
+                    cmd.CommandText = @"Insert Into Tbl_Personel(P_Adi,P_Soyadi,P_TcKimlik,P_Tel1,P_Tel2,P_Email,P_Cinsiyet,P_D_Tar,P_D_Yer,P_Pozisyon,P_Dept,P_Med_Hal,P_Aday) 
                                  values (@P_Adi,@P_Soyadi,@P_TcKimlik,@P_Tel1,@P_Tel2,@P_Email,@P_Cinsiyet,@P_D_Tar,@P_D_Yer,@P_Pozisyon,@P_Dept,@P_Med_Hal,@P_Aday)";
-                string mf,mh,ady;
-                
-                if (cinsiyet.Text == "Bayan")
-                {
-                    mf = "0";
-                }else{
-                    mf = "1";
+                    string mf, mh, ady;
+
+                    if (cinsiyet.Text == "Bayan")
+                    {
+                        mf = "0";
+                    }
+                    else
+                    {
+                        mf = "1";
+                    }
+                    if (medenihal.Text == "Evli")
+                    {
+                        mh = "1";
+                    }
+                    else
+                    {
+                        mh = "0";
+                    }
+                    ady = (adaydurumu.Text == "Aday") ? "0" : "1";
+
+
+
+
+                    cmd.Parameters.AddWithValue("@P_Adi", isim.Text);
+                    cmd.Parameters.AddWithValue("@P_Soyadi", soyisim.Text);
+                    cmd.Parameters.AddWithValue("@P_TcKimlik", tckimlik.Text);
+                    cmd.Parameters.AddWithValue("@P_Tel1", telefonno.Text);
+                    cmd.Parameters.AddWithValue("@P_Tel2", ceptelefon.Text);
+                    cmd.Parameters.AddWithValue("@P_Email", email.Text);
+                    cmd.Parameters.AddWithValue("@P_Cinsiyet", mf);
+                    cmd.Parameters.AddWithValue("@P_D_Tar", dogumtarihi.SelectedDate.Value);
+                    cmd.Parameters.AddWithValue("@P_D_Yer", dogumyeri.Text);
+                    cmd.Parameters.AddWithValue("@P_Pozisyon", pozisyon.Text);
+                    cmd.Parameters.AddWithValue("@P_Dept", departman.Text);
+                    cmd.Parameters.AddWithValue("@P_Med_Hal", mh);
+                    cmd.Parameters.AddWithValue("@P_Aday", ady);
+
+
+                    cmd.ExecuteNonQuery();
+
+                    MessageBox.Show("Kayıt Yapıldı..");
+                    this.Hide();
+                    PersonelEkleSil ek = new PersonelEkleSil(1);
+                    ek.Show();
+                    con.Close();
+
+
+
                 }
-                if (medenihal.Text == "Evli")
-                {
-                    mh = "1";
-                }else{
-                    mh ="0";
-                }
-                ady = (adaydurumu.Text == "Aday") ? "0" : "1";
-
-                
-
-
-                cmd.Parameters.AddWithValue("@P_Adi", isim.Text);
-                cmd.Parameters.AddWithValue("@P_Soyadi", soyisim.Text);
-                cmd.Parameters.AddWithValue("@P_TcKimlik", tckimlik.Text);
-                cmd.Parameters.AddWithValue("@P_Tel1", telefonno.Text);
-                cmd.Parameters.AddWithValue("@P_Tel2", ceptelefon.Text);
-                cmd.Parameters.AddWithValue("@P_Email", email.Text);
-                cmd.Parameters.AddWithValue("@P_Cinsiyet", mf);
-                cmd.Parameters.AddWithValue("@P_D_Tar", dogumtarihi.SelectedDate.Value);
-                cmd.Parameters.AddWithValue("@P_D_Yer", dogumyeri.Text);
-                cmd.Parameters.AddWithValue("@P_Pozisyon", pozisyon.Text);
-                cmd.Parameters.AddWithValue("@P_Dept", departman.Text);
-                cmd.Parameters.AddWithValue("@P_Med_Hal", mh);
-                cmd.Parameters.AddWithValue("@P_Aday", ady);
-                
-
-                cmd.ExecuteNonQuery();
-
-                MessageBox.Show("Kayıt Yapıldı..");
-                this.Hide();
-                PersonelEkleSil ek = new PersonelEkleSil(1);
-                ek.Show();
-                con.Close();
-
-
-
+            }
+            catch
+            {
+                MessageBox.Show("Lütfen tüm alanları doldurunuz");
             }
 
 
