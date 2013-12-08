ea19d6088e9a62b77ec796ecff6b901929a5a8eb
m1u2s3t4 <mustafa5555588@mynet.com>
2013-12-08 22:46:13 +0200

    WDokuman try-catch

--- WpfApplication1/WpfApplication1/Wdokuman.xaml.cs
+++ WpfApplication1/WpfApplication1/Wdokuman.xaml.cs
@@ -24,12 +24,47 @@ namespace WpfApplication1
         public Wdokuman()
         {
             InitializeComponent();
+           
         }
 
         private void dokumanIslem_Click(object sender, RoutedEventArgs e)
         {
             DokumanListesi lst = new DokumanListesi();
             lst.Show();
+            try
+            {
+                selected_dokuman = sid;
+                InitializeComponent();
+                SqlCommand cmd = new SqlCommand();
+                con.ConnectionString = yet.ki.con;
+                con.Open();
+                cmd.Connection = con;
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "Select * From Tbl_Personel   ";
+                cmd.Parameters.AddWithValue("@id", sid);
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    PersonelAdi.Text = writer["P_Adi"].ToString();
+                    PersonelSoyadi.Text = writer["P_Soyadi"].ToString();
+                    Tc.Text = writer["P_TcKimlik"].ToString();
+                    Tel1.Text = writer["P_Tel1"].ToString();
+                    tel2.Text = writer["P_Tel2"].ToString();
+                    Email.Text = writer["P_Email"].ToString();
+                }
+                con.Close();
+                sadi.Text = sir.name;
+                sadi.Text = sir.lname;
+                stel.Text = sir.tel;
+                Email.Text = sir.email;
+                sweb.Text = sir.web;
+                logoS.Source = sir.ket;
+            }
+            catch
+            {
+                MessageBox.Show("Dokuman Secme Sirasinda Bir Hata Olustu");
+            }
+
             
         }
 
